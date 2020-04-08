3d7bcdd14185f06efd27fdb71d79734919852c3a
Terrence Lim <hlim1@email.arizona.edu>
2020-04-08 13:53:10 -0700

    Modified settings to use static files

--- stockAlarm/src/settings.py
+++ stockAlarm/src/settings.py
@@ -5,8 +5,10 @@ BASE_DIR = os.path.dirname(os.path.abspath("stockAlarm"))
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"stockAlarm/src/static"),
+    os.path.join(BASE_DIR,"stockAlarm/static"),
     # '/var/www/static/',
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "stockAlarm/src")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "stockAlarm")
+
+DEBUG=False
